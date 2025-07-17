610a4e303ad69e231d52ab081b56ec5f7d9fd1ba
pramodhhirushan2000 <pramodhhirushan24@gmail.com>
2025-07-17 12:56:40 +0530

    Add files via upload

---
+++ Services/FakeEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace DreamDay.Services
+{
+    public class FakeEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Just return completed task (no real email sending)
+            return Task.CompletedTask;
+        }
+    }
+}
